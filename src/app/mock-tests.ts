7b27eb3b8f624088a5dbfd94809b5890dce02e9f
deogemini <geminideogratias8@gmail.com>
2021-11-09 23:53:26 +0300

    addition of mock-file

---
+++ src/app/mock-tests.ts
@@ -0,0 +1,20 @@
+export const TASK = [
+  {
+    id: 1,
+    text: 'Doctors Appoitment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'May 5th at 2:30pm',
+    reminder: false,
+  },
+];
