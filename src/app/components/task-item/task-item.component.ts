f1bc10ebd8e4c459fa69234f22623c3965a4ea95
deogemini <geminideogratias8@gmail.com>
2021-11-11 05:38:42 +0300

    setting reminder by toggle function added

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -6,21 +6,23 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+  styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onTogglerReminder: EventEmitter<Task> = new EventEmitter();
+
   faTimes = faTimes;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onDelete(task){
+  onDelete(task: Task | undefined) {
     this.onDeleteTask.emit(task);
-
   }
-
+  onToggle(task: Task | undefined) {
+    this.onTogglerReminder.emit(task);
+  }
 }
